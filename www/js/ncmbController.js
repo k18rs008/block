84923a88814f405479f427c1ff3bfcbd0212bcdd
k18rs008 <k18rs008@st.kyusan-u.ac.jp>
2019-11-25 14:20:27 +0900

    順位表示機能を追加

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -17,7 +17,21 @@ var ncmbController = {
 
     scoreData.save()
     .then(function (saved) {
-      alert("スコア送信完了！");
+      Score.greaterThan("score", score)
+      .count()
+      .fetchAll()
+      .then(function(scores){
+        var rank = (scores.count !== undefined) ? parseInt(scores.count) + 1 : 1;
+        if(typeof navigator.notification !== 'undefined'){
+          navigator.notification.alert(
+            "今回の順位は #" + rank + " でした！",
+            function(){},
+            "スコア送信完了！"
+          );
+        } else {
+          alert("スコア送信完了！\n今回の順位は #" + rank + " でした！");
+        }
+      })
     })
     .catch(function(err){
       console.log(err);
