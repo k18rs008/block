b0ca16bb5cb4dee110af8fa21e6754205c92b825
k18rs008 <k18rs008@st.kyusan-u.ac.jp>
2019-11-25 14:14:25 +0900

    スコア保存機能を追加

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -8,6 +8,22 @@ var ncmbController = {
   currentUser: null,
   screenSize: null,
 
+  sendScore: function(score) {
+    var self = this;
+
+    var Score = self.ncmb.DataStore("ScoreClass");
+    
+    var scoreData = new Score({score: score});
+
+    scoreData.save()
+    .then(function (saved) {
+      alert("スコア送信完了！");
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+},
+
   init: function(screenSize) {
     var self = this;
     self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);
